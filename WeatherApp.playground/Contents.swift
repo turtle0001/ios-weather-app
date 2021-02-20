435282b935f3efa82d917d4d4c33bd6e89d7beaa
Joash Tubaga <joash.tubaga@bell.ca>
2021-02-21 01:13:45 +0800

    Added code to handle weather API model

---
+++ WeatherApp.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+struct Forecast: Codable {
+    let daily:[Daily]
+}
+
+struct Daily: Codable {
+    let dt: Date
+    let temp: Temp
+    let humidity: Int
+    let weather: [Weather]
+    let clouds: Int
+    let pop: Double
+    
+}
+
+struct Temp: Codable {
+    let min: Double
+    let max: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    let description: String
+    let icon: String
+    var weatherIconURL: URL {
+        let urlString = "http://openweathermap.org/img/wn/\(icon)@2x.png"
+        return URL(string: urlString)!
+    }
+}
