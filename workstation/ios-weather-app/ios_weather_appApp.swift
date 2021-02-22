f2d8e79d5887d65510944501ab7f43801c0ff6ea
Joash Tubaga <joash.tubaga@bell.ca>
2021-02-22 14:28:13 +0800

    initial commit

---
+++ workstation/ios-weather-app/ios_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_weather_appApp.swift
+//  ios-weather-app
+//
+//  Created by joash.tubaga on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
